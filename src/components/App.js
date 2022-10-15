c2b8e3b305aacb8900940f3bb6098ab28be37f88
Hyfa-Hassan <110564591+Hyfa-Hassan@users.noreply.github.com>
2022-10-15 20:43:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
+const a = Now I can render any React component on any DOM node I want using ReactDOM.render;
 function App() {
   return (
     <>
-    <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    <p>{a}</p>
     </>
   );
 }
