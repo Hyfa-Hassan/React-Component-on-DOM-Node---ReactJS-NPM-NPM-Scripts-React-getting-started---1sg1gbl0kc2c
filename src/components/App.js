48c6dcbcdb72bcb0085147dbc46fd306cd16d574
Hyfa-Hassan <110564591+Hyfa-Hassan@users.noreply.github.com>
2022-10-15 22:58:41 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,12 @@
-function App() {
-    ReactDOM.render(<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>, document.getElementById('root'));  
+// import React, {Component, useState} from "react";
+// import '../styles/App.css';
+
+const App = () => {
+  return (
+    <div>
+    <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+  )
 }
+
 export default App;
